bedcc06f319c862e13baa78c84f8e2a802c56cfd
David Estes <davidestes@stripe.com>
2020-08-24 13:25:26 -0700

    Commit test package for SwiftPM

---
+++ Package.swift
@@ -0,0 +1,61 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "Stripe",
+  platforms: [
+    .iOS(.v10)
+  ],
+  products: [
+    .library(
+      name: "Stripe",
+      targets: ["StripeiOS"]
+    ),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .binaryTarget(
+      name: "StripeiOS",
+      url: "https://github.com/stripe/stripe-ios/releases/download/v19.4.0/Stripe.xcframework.zip",
+        checksum: "d1ab10f4fbd5d577715a002309136e7b17a400c92d78151a8ea331d160c04b20"
+    ),
+  ]
+)
+
+
+/*
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "Stripe",
+  platforms: [
+    .iOS(.v10)
+  ],
+  products: [
+    .library(
+      name: "Stripe",
+      targets: ["StripeiOS", "libStripe3DS2"]
+    ),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(
+      name: "StripeiOS",
+      path: "Stripe",
+      publicHeadersPath: "Stripe/PublicHeaders"
+    ),
+    .testTarget(
+      name: "StripeiOS Tests",
+      dependencies: ["StripeiOS", "libStripe3DS2"],
+      path: "Tests"
+    ),
+    .binaryTarget(
+      name: "libStripe3DS2",
+      path: "InternalFrameworks/libStripe3DS2.a"
+    ),
+  ]
+)
+*/
